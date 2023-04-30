8e5ef572cd42a9101385f9f9cfb16afa078ef76b
Shubham Tyagi <shubhamtyagi1306@gmail.com>
2023-04-30 14:07:56 +0530

    create variables for score, dice and button class in js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 'use strict';
+
+// Selecting elements
+const score0El = document.querySelector('#score--0');
+const score1El = document.getElementById('score--1');
+const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn-new');
+const btnRoll = document.querySelector('.btn-roll');
+const btnHold = document.querySelector('.btn-hold');
+
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add('hidden');
